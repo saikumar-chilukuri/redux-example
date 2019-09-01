c9b9ce6848e9e2aadc742774d20267f36a67a08a
Sai kumar Chilukuri <nsaikumar.ch@gmail.com>
2019-09-01 16:38:41 +0530

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+    return [
+        {'title': 'Book1'},
+        {'title': 'Book2'},
+        {'title': 'Book3'},
+        {'title': 'Book4'},
+        {'title': 'Book5'},
+        {'title': 'Book6'}
+    ]
+}
